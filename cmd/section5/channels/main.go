143e1e48ff4bb6076e09df79c1e03ffd26423ebb
Wojciech Fabjańczuk <wojciechfabjanczuk@gmail.com>
2022-09-19 12:34:43 +0200

    section5: ping pong

---
+++ cmd/section5/channels/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	ping := make(chan string)
+	pong := make(chan string)
+
+	go shout(ping, pong)
+	pingPong(ping, pong)
+
+	fmt.Println("--------------------------")
+	fmt.Println("All done. Closing channels")
+	close(ping)
+	close(pong)
+}
+
+func pingPong(ping chan<- string, pong <-chan string) {
+	fmt.Println("Type something and press ENTER (or Q to quit)")
+
+	for {
+		fmt.Print("-> ")
+		var userInput string
+		_, _ = fmt.Scanln(&userInput)
+
+		if strings.ToLower(userInput) == "q" {
+			return
+		}
+
+		ping <- userInput
+		response := <-pong
+
+		fmt.Println("\tResponse:", response)
+	}
+}
+
+func shout(ping <-chan string, pong chan<- string) {
+	for {
+		s := <-ping
+		pong <- fmt.Sprintf("%s!!!", strings.ToUpper(s))
+	}
+}
